97d527d1c65e3f9ca3e9132022e0978fc762dcd9
Egotistical Bastard <egobstd@gmail.com>
2011-06-01 13:37:39 +0200

    Add command to only get the env shell cmd

--- poc.py
+++ poc.py
@@ -5,14 +5,19 @@ import subprocess
 import argparse
 from datetime import datetime
 
+
+def makeenv(date):
+    datestring = date.strftime(r'%Y-%m-%dT%H:%M:%S')
+    return {'GIT_COMMITTER_DATE': datestring, 'GIT_AUTHOR_DATE': datestring}
+
+
 def commit(date):
     env = os.environ.copy()
-    datestring = date.strftime(r'%Y-%m-%dT%H:%M:%S')
-    env["GIT_COMMITTER_DATE"] = datestring
-    env["GIT_AUTHOR_DATE"] = datestring
+    env.update(makeenv(date))
     subprocess.check_call(["/usr/bin/env", "git", "commit"], env=env)
 
-def get_last_commit_datetime(author = None):
+
+def get_last_commit_datetime(author=None):
     args = ["/usr/bin/env", "git", "log",
             "--color=never", "--pretty=%at", "--date=local", "-F",
             "--all", "--max-count=1", "--date-order"]
@@ -23,27 +28,33 @@ def get_last_commit_datetime(author = None):
 
     return datetime.fromtimestamp(timestamp)
 
+
 if __name__ == '__main__':
     now = datetime.today()
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers()
-    parser_commit = subparsers.add_parser('commit', help='Commit at a different date and time.')
+    parser_commit = subparsers.add_parser('fakedate',
+            help='Commit at a different date and time.')
     parser_commit.add_argument('-y', '--year', type=int, default=now.year)
     parser_commit.add_argument('-m', '--month', type=int, default=now.month)
     parser_commit.add_argument('-d', '--day', type=int, default=now.day)
     parser_commit.add_argument('-j', '--hour', type=int, default=now.hour)
     parser_commit.add_argument('-i', '--minute', type=int, default=now.minute)
     parser_commit.add_argument('-s', '--second', type=int, default=now.second)
-    parser_commit.set_defaults(action="commit")
-    parser_lastcommit = subparsers.add_parser('lastcommit', help='Get the last commit date and time.')
+    parser_commit.add_argument('-a', '--action', default="commit")
+    parser_lastcommit = subparsers.add_parser('lastcommit',
+            help='Get the last commit date and time.')
     parser_lastcommit.set_defaults(action="lastcommit")
     args = parser.parse_args()
     action = args.action
 
-    if action == "commit":
+    if action == "commit" or action == "getenv":
         fakedt = datetime(year=args.year, month=args.month, day=args.day,
                         hour=args.hour, minute=args.minute, second=args.second)
         print "Fake date: %s" % fakedt
-        commit(fakedt)
+        if action == "commit":
+            commit(fakedt)
+        if action == "getenv":
+            print ' '.join(['%s="%s"' % (k, v) for k, v in makeenv(fakedt).iteritems()])
     elif action == "lastcommit":
         print "Last commit at: %s" % get_last_commit_datetime()
